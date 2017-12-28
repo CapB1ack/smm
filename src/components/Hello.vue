d34636eb0be08e64f7c83474c78b3dca410a2352
CapB1ack <capback250@gmail.com>
2017-12-28 08:11:59 +0300

    header

--- src/components/Hello.vue
+++ src/components/Hello.vue
@@ -1,12 +1,15 @@
 <template>
   <q-layout>
-    <div class="background">
-      <img :src='bgPics[bgPicsIndex]' alt="">
-    </div>
-    <div class="f1"></div>
-    <div class="f2"></div>
-    <div class="f3"></div>
-    <div class="f4"></div>
+    <header class="header">
+      <div class="header-top" :class="{'header-top-hidden': scrolled}">
+        <div class="row justify-around items-center full">
+          <div>123123123</div>
+          <div>123123123</div>
+        </div>
+      </div>
+      <div class="header-fixed" :class="{'header-fixed-hidden': scrolled}"></div>
+    </header>
+    <div v-for="x in 5" style="height: 100vh"></div>
   </q-layout>
 </template>
 <script>
@@ -18,48 +21,36 @@
     },
     data () {
       return {
-        bgPics: [
-          'https://hsto.org/files/39e/0b3/fa8/39e0b3fa81974ba59b1482a3e38b802f.png',
-          'http://adapto.rs/arc/site/data/uploads/newsimg/github-logo.jpg'
-        ],
-        bgPicsIndex: 0
+        scrolled: false
       }
     },
     created () {
-      window.addEventListener('wheel', this.handleWheel)
+      window.addEventListener('scroll', this.handleScroll)
     },
     methods: {
-      handleWheel (e) {
-        console.log(window.screen.height, window.pageYOffset)
-        if (window.screen.height < window.pageYOffset && this.bgPicsIndex === 0) {
-          this.bgPicsIndex = 1
-        }
-        else if (window.screen.height > window.pageYOffset && this.bgPicsIndex !== 0) {
-          this.bgPicsIndex = 0
-        }
+      handleScroll() {
+        this.scrolled = window.scrollY > 0;
       }
     }
   }
 </script>
 <style lang="stylus">
-  .background
+  .header
+    width: 100vw
     position fixed
-    z-index -1
-    width 100vw
-    height 100vh
-    img
-      width 100%
-      height 100%
-  .f1
-    height 100vh
-    background-color rgba(1, 255, 47, 0.2)
-  .f2
-    height 100vh
-    background-color rgba(173, 255, 47, 0.2)
-  .f3
-    height 100vh
-    background-color pink
-  .f4
-    height 100vh
-    background-color teal
+    &-top
+      height: 3rem
+      background-color #fff
+      transition all .1s ease
+      &-hidden
+        transform translateY(-100%)
+    &-fixed
+      height: 7rem;
+      background-color #2a353c
+      transition all .1s ease
+      &-hidden
+        transform translateY(-3rem)
+  .full
+    height 100%
+    width 100%
 </style>
